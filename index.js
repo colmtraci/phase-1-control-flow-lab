4f17876669b47af127fa18e46d9ef32e95bbbe5f
Colm Traci <ctraci@me.com>
2023-12-12 16:03:02 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDist){
   // Write your code here!
+  if (rideDist <= 400){
+    return 'This one is on me!'
+  } else if (rideDist > 400 && rideDist <= 2000){
+    return 'That will be twenty bucks.'
+  } else if (rideDist > 2000 && rideDist <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (rideDist >= 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == 'NYC'){
+    return 'Ok, sounds good.'
+  } else if (city != 'NYC'){
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(gen){
   // Write your code here!
+  switch (true){
+    case gen == 'generous':
+      return 'Thank you so much.';
+    case gen == 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
